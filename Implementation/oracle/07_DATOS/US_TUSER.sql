b386f082ef6a70ab2f1b80fbbd32206007bac59e
unknown <you@example.com>
2018-05-15 11:15:56 -0500

    crack

--- Implementation/oracle/07_DATOS/US_TUSER.sql
+++ Implementation/oracle/07_DATOS/US_TUSER.sql
@@ -1,6 +1,9 @@
 --------------------------------------------------------
---  File created - Monday-May-14-2018   
+-- Archivo creado  - martes-mayo-15-2018   
 --------------------------------------------------------
 REM INSERTING into FS_AUWEB_US.US_TUSER
 SET DEFINE OFF;
-Insert into FS_AUWEB_US.US_TUSER (USER_USER,USER_ALAS,USER_PSWD) values (1,'crack','F<?????');
+Insert into FS_AUWEB_US.US_TUSER (USER_USER,USER_ALAS,USER_PSWD) values ('1','master-zen','F<?????');
+Insert into FS_AUWEB_US.US_TUSER (USER_USER,USER_ALAS,USER_PSWD) values ('2','proplayer',' ,?b?[?-#Kp');
+Insert into FS_AUWEB_US.US_TUSER (USER_USER,USER_ALAS,USER_PSWD) values ('3','prokotan',' ,?b?[?-#Kp');
+COMMIT;
